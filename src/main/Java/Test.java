aaccb62308a7e21fae3ff9ade38cb99474412039
Tgc19970303 <1052843633@qq.com>
2019-10-22 16:46:07 +0800

    陶大大增加Test类

---
+++ src/main/Java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("阿桑大事大asdas打色");
+
+        System.out.println("按时打撒啊谁都爱见你上技能");
+
+
+    }
+
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World但是舒服的方式地方!</h2>
+
 </body>
 </html>
